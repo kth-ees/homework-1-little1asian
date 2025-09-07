5752fb2956348eafbbea4f18c1f33558d75d7dec
datra <149829163+little1asian@users.noreply.github.com>
2025-09-07 14:53:14 +0200

    HW1

--- q1/decoder_tb.sv
+++ q1/decoder_tb.sv
@@ -10,5 +10,24 @@ module decoder_tb;
     .one_hot(one_hot)
   );
 
-  // Complete your testbench here
+  initial begin
+    binary = 4'b0000; #10;
+    binary = 4'b0001; #10;
+    binary = 4'b0010; #10;
+    binary = 4'b0011; #10;
+    binary = 4'b0100; #10;
+    binary = 4'b0101; #10;
+    binary = 4'b0110; #10;
+    binary = 4'b0111; #10;
+    binary = 4'b1000; #10;
+    binary = 4'b1001; #10;
+    binary = 4'b1010; #10;
+    binary = 4'b1011; #10;
+    binary = 4'b1100; #10;
+    binary = 4'b1101; #10;
+    binary = 4'b1110; #10;
+    binary = 4'b1111; #10;
+
+    $finish;
+  end
 endmodule
